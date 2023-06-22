b9bdff6fad93197c0421b40177d6e62e679085db
Redither <slavonchik68@gmail.com>
2023-06-22 21:41:30 +0300

    save for migration to another WS

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "layouts#index"
+  get  "/layouts", to: "layouts#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
